0f8d718c06b7e8093ab356ec8ddf9c132634310b
SMeydan <sudenurmeydan01@gmail.com>
2024-02-22 20:57:31 +0300

    Added day 3

---
+++ 100_days_of_cs/day-3-life-in-weeks/WeekInLifeApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Welcome, please enter your age:");
+int age  = int.Parse(Console.ReadLine());
+averageLifeSpan = 90;
+weeksInLife = (averageLifeSpan - age) * 52;
+daysInLife = (averageLifeSpan - age) * 365;
+hoursInLife = daysInLife * 24;
+minutesInLife = hoursInLife * 60;
+secondsInLife = minutesInLife * 60;
+Console.WriteLine($"You have {weeksInLife} weeks, {daysInLife} days, {hoursInLife} hours, {minutesInLife} minutes and {secondsInLife} seconds left in your life.");
